b03624505ddd98f5e4263b5761f5da954593960a
UETCodeFun <xuansonpham00@gmail.com>
2022-03-07 00:06:48 +0700

    new project

---
+++ Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPad_SonPX
+{
+    class Document
+    {
+        public bool IsEdit { get; set; } = false;
+        public string PathDocument { get; set; } = null;
+        public string getNameDoc()
+        {
+            if(PathDocument != String.Empty)
+            {
+                return Path.GetFileNameWithoutExtension(PathDocument);
+            }
+            return String.Empty;
+        }
+    }
+}
